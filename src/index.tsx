641d03e1cac6906f65312cc3fa3343395b4c7d0c
Andrew Serbin <95167390+serbinadrian@users.noreply.github.com>
2025-08-08 13:51:51 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import App from "@components/App";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot, Root } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement: HTMLElement | null = document.getElementById("root");
+
+if (!rootElement) {
+    throw new Error("Cannot acquire #root element, Aborting...");
+}
+
+const reactRoot: Root = createRoot(rootElement);
+
+reactRoot.render(
     <StrictMode>
         <App />
     </StrictMode>
